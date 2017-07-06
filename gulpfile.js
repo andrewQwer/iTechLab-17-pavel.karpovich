85f31d652026b7cda7a20e15fdbfaf7bd6b9bee5
Pavel Karpovich <tallerstk97@gmail.com>
2017-07-06 15:46:43 +0300

    add responsive header to the HTML/CSS task

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-    gulp.src('1. HTML-CSS/1.2 Flex-box/sass/**/*.scss')
+    gulp.src('1. HTML-CSS/1.3. Responsive/sass/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('1. HTML-CSS/1.2 Flex-box/styles'));
+        .pipe(gulp.dest('1. HTML-CSS/1.3. Responsive/styles'));
 });
