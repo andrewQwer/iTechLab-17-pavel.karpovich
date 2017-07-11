898af50e531c5e419f437bc56869bba8e0185c81
Pavel Karpovich <tallerstk97@gmail.com>
2017-07-11 17:35:16 +0300

    add header (not full)

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-    gulp.src('1. HTML-CSS/1.3. Responsive/sass/**/*.scss')
+    gulp.src('1. HTML-CSS/1.4. BEM/sass/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('1. HTML-CSS/1.3. Responsive/styles'));
+        .pipe(gulp.dest('1. HTML-CSS/1.4. BEM/styles'));
 });
