bcef818c9f03e1331936982a8ae0913fea8bfc4c
taller_stk <tallerstk97@gmail.com>
2017-07-22 21:04:58 +0300

    was added first function to js advanced task

---
+++ 3. JS_Advance/Tasks/First/js/First.js
@@ -0,0 +1,11 @@
+(function(){
+	let First = (array, predicate) => {
+		for(let item of array) {
+			if(predicate.call(null, item))
+				return item;
+		}
+	}
+
+	var result = First([1,2,3,4,5], (item) => item > 3);
+	console.log(result);
+}())
