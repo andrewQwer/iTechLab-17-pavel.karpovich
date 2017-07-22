e203f4191c3c221ce60fdc13776625746532cfe0
taller_stk <tallerstk97@gmail.com>
2017-07-22 15:58:46 +0300

    was added map function to js advanced task

---
+++ 3. JS_Advance/Tasks/Map/js/Map.js
@@ -0,0 +1,12 @@
+(function(){
+	function Map(array, callback){
+		let resultArray = [];
+		for(let item of array) {
+			resultArray.push(callback.call(null, item));
+		}
+		return resultArray;
+	}
+
+	var lengthArray = Map(["liza", "taller", "maax"], (item)=>item.length);
+	console.log(lengthArray);
+})();
