441c9cac36ce78015f566fbeb59be6b881cb504a
taller_stk <tallerstk97@gmail.com>
2017-07-26 17:51:55 +0300

    SQL
    was created and filled tables

---
+++ 5. SQL/CreateTables.sql
@@ -0,0 +1,47 @@
+USE master;
+GO
+
+CREATE DATABASE Company
+GO
+
+USE Company
+GO
+
+CREATE TABLE Department (
+  Id      INT PRIMARY KEY IDENTITY (1, 1),
+  Title   NVARCHAR(60) UNIQUE NOT NULL CHECK (Title != ''),
+  Address NVARCHAR(60)        NOT NULL CHECK (Address != '')
+);
+
+CREATE TABLE Employee (
+  Id        INT PRIMARY KEY IDENTITY (1, 1),
+  FirstName NVARCHAR(60) NOT NULL CHECK (FirstName != ''),
+  LastName  NVARCHAR(60) NOT NULL CHECK (LastName != ''),
+  FullName                  AS FirstName + ' ' + LastName,
+  Birthday  DATETIME     NOT NULL
+);
+
+CREATE TABLE Job (
+  Id        INT PRIMARY KEY IDENTITY (1, 1),
+  Position  NVARCHAR(100),
+  MinSalary MONEY NOT NULL CHECK (MinSalary > 0)
+);
+
+CREATE TABLE Career (
+  JobId          INT REFERENCES Job (Id),
+  EmployeeId     INT REFERENCES Employee (Id),
+  DepartmentId   INT REFERENCES Department (Id),
+  EmploymentDate DATETIME NOT NULL,
+  DismissalDate  DATETIME
+);
+
+CREATE TABLE Salary (
+  EmployeeId INT REFERENCES Employee (Id),
+  Month      TINYINT  NOT NULL CHECK (Month BETWEEN 1 AND 12),
+  Year       SMALLINT NOT NULL CHECK (Year BETWEEN 2002 AND 2017),
+  Salary     MONEY    NOT NULL
+);
+
+SELECT *
+FROM Job
+
---
+++ 5. SQL/Insert/Career.sql
@@ -0,0 +1,204 @@
+USE Company;
+GO
+
+
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (70, 40, 63, '2011-01-12 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (57, 13, 51, '2013-09-09 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (44, 43, 75, '2004-06-04 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (87, 65, 81, '2011-12-11 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (80, 37, 55, '2014-12-01 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (55, 58, 93, '2006-12-06 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (1, 96, 8, '2016-12-10 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (10, 68, 68, '2003-12-09 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (73, 19, 47, '2009-06-04 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (27, 65, 6, '2011-02-08 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (33, 1, 28, '2010-11-07 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (83, 30, 21, '2006-12-01 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (99, 29, 74, '2007-02-02 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (84, 44, 12, '2003-12-10 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (26, 6, 4, '2009-02-01 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (20, 34, 66, '2008-11-04 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (60, 50, 58, '2011-12-03 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (86, 22, 59, '2011-01-09 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (26, 74, 12, '2012-12-03 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (31, 26, 62, '2005-12-07 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (85, 26, 54, '2006-12-02 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (68, 7, 62, '2010-12-11 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (1, 1, 41, '2006-12-03 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (78, 1, 31, '2004-12-06 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (53, 88, 41, '2006-04-08 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (98, 11, 45, '2014-12-09 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (100, 46, 24, '2014-12-03 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (26, 87, 14, '2017-12-02 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (42, 53, 44, '2013-08-11 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (13, 82, 28, '2008-12-08 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (53, 63, 35, '2011-03-09 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (93, 8, 38, '2003-05-05 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (33, 94, 34, '2012-12-07 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (28, 72, 62, '2007-08-04 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (73, 32, 46, '2004-12-03 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (14, 40, 81, '2005-12-01 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (33, 77, 10, '2013-12-12 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (100, 71, 50, '2010-12-03 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (13, 28, 71, '2007-02-07 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (84, 74, 76, '2011-12-09 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (68, 89, 43, '2014-12-02 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (35, 52, 90, '2009-12-08 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (58, 79, 3, '2008-08-08 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (59, 38, 17, '2008-12-04 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (54, 8, 3, '2015-12-12 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (25, 91, 98, '2003-07-06 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (63, 21, 80, '2015-04-11 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (5, 17, 3, '2016-12-05 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (22, 51, 68, '2009-12-02 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (53, 80, 20, '2003-12-12 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (79, 11, 10, '2009-05-07 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (88, 97, 6, '2005-12-07 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (32, 4, 13, '2010-12-01 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (88, 24, 17, '2016-09-10 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (53, 99, 29, '2015-12-10 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (68, 15, 46, '2012-10-10 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (46, 75, 85, '2017-11-05 00:00:00.000', NULL);
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (44, 32, 82, '2002-12-09 00:00:00.000', '2002-01-09 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (27, 18, 26, '2013-10-05 00:00:00.000', '2002-07-09 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (70, 50, 64, '2004-12-05 00:00:00.000', '2002-09-10 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (7, 28, 34, '2004-12-03 00:00:00.000', '2004-07-07 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (35, 56, 20, '2010-03-12 00:00:00.000', '2004-12-01 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (24, 30, 58, '2016-02-10 00:00:00.000', '2004-12-06 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (59, 23, 75, '2010-05-01 00:00:00.000', '2004-12-08 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (22, 76, 75, '2017-10-07 00:00:00.000', '2004-12-10 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (26, 100, 72, '2009-02-02 00:00:00.000', '2005-11-11 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (72, 70, 11, '2004-12-06 00:00:00.000', '2005-12-01 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (62, 73, 64, '2003-12-12 00:00:00.000', '2005-12-03 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (2, 68, 63, '2012-06-12 00:00:00.000', '2005-12-03 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (88, 97, 82, '2005-02-09 00:00:00.000', '2005-12-05 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (83, 50, 41, '2003-06-12 00:00:00.000', '2005-12-06 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (37, 82, 49, '2013-12-07 00:00:00.000', '2005-12-07 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (39, 38, 15, '2006-07-03 00:00:00.000', '2006-06-08 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (60, 18, 44, '2015-12-09 00:00:00.000', '2006-12-04 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (38, 95, 9, '2015-12-02 00:00:00.000', '2006-12-05 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (99, 48, 93, '2015-12-08 00:00:00.000', '2007-02-04 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (33, 7, 57, '2008-08-10 00:00:00.000', '2007-09-07 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (32, 82, 81, '2015-12-07 00:00:00.000', '2007-12-03 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (13, 64, 52, '2011-01-11 00:00:00.000', '2007-12-07 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (39, 92, 93, '2007-12-06 00:00:00.000', '2008-12-02 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (6, 92, 9, '2005-12-04 00:00:00.000', '2008-12-07 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (18, 72, 71, '2011-08-07 00:00:00.000', '2008-12-07 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (91, 38, 66, '2006-06-11 00:00:00.000', '2009-07-01 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (35, 98, 63, '2006-12-12 00:00:00.000', '2009-12-03 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (78, 55, 67, '2007-12-04 00:00:00.000', '2009-12-04 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (61, 78, 22, '2015-12-11 00:00:00.000', '2011-12-12 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (88, 64, 80, '2008-04-04 00:00:00.000', '2012-10-01 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (34, 31, 15, '2013-12-05 00:00:00.000', '2013-12-07 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (47, 26, 19, '2014-12-03 00:00:00.000', '2013-12-07 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (76, 92, 16, '2008-11-08 00:00:00.000', '2013-12-11 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (36, 21, 33, '2006-12-11 00:00:00.000', '2013-12-11 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (28, 12, 59, '2004-12-03 00:00:00.000', '2015-10-03 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (68, 78, 54, '2012-04-11 00:00:00.000', '2015-12-06 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (86, 68, 35, '2004-12-10 00:00:00.000', '2016-07-03 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (40, 36, 25, '2004-12-03 00:00:00.000', '2016-09-02 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (96, 75, 98, '2014-12-10 00:00:00.000', '2016-12-05 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (85, 83, 30, '2012-12-08 00:00:00.000', '2017-06-04 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (11, 13, 55, '2010-12-09 00:00:00.000', '2017-09-02 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (58, 72, 29, '2004-07-07 00:00:00.000', '2017-12-03 00:00:00.000');
+INSERT INTO Career (JobId, EmployeeId, DepartmentId, EmploymentDate, DismissalDate)
+VALUES (71, 32, 1, '2012-12-03 00:00:00.000', '2017-12-04 00:00:00.000');
